0aaacae868abf16ea9910146861df7c202c6ce39
David Madaras <madaras.david1@gmail.com>
2018-01-29 22:17:52 +0100

    charts

--- QuizMobileApp/QuizMobileApp/View/LevelPlayPage.xaml.cs
+++ QuizMobileApp/QuizMobileApp/View/LevelPlayPage.xaml.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microcharts.Forms;
+using Microcharts;
+using SkiaSharp;
 
 namespace QuizMobileApp.View
 {
@@ -17,7 +20,8 @@ namespace QuizMobileApp.View
 
         public LevelPlayViewModel LevelViewModel;
         public JokersModel Jokers {get;set;}
-        private Random _rnd; 
+        private Random _rnd;
+        private List<OptionInQuestionModel> actualOptions;
 
 
         //because preview
@@ -33,8 +37,114 @@ namespace QuizMobileApp.View
             _rnd = new Random();
             LevelViewModel = lvlVM;
             LblQuestion.Text = LevelViewModel.GetActualQuestion().QuestionText;
-            listOptions.ItemsSource = LevelViewModel.GetActualQuestion().Options.OrderBy((item)=>_rnd.Next());
+            actualOptions = LevelViewModel.GetActualQuestion().Options.OrderBy((item) => _rnd.Next()).ToList();
+            listOptions.ItemsSource = actualOptions;
             listOptions.ItemClickCommand = ItemClickCommand;
+
+            
+            var fiftyTapRecognizer = new TapGestureRecognizer
+            {
+                Command = new Command(OnFiftyFifty),
+                NumberOfTapsRequired = 1
+            };
+            fiftyfiftyImg.GestureRecognizers.Add(fiftyTapRecognizer);
+
+            var peopleTapRecognizer = new TapGestureRecognizer
+            {
+                Command = new Command(OnPeopleTap),
+                NumberOfTapsRequired = 1
+            };
+            peopleImg.GestureRecognizers.Add(peopleTapRecognizer);
+        }
+
+        private void OnPeopleTap(object obj)
+        {
+            var opti = actualOptions;
+            var count = opti.Count;
+            List<int> randomVoting = new List<int>();
+            List<Microcharts.Entry> entries = new List<Microcharts.Entry>();
+            for (int i = 0; i < count; i++)
+            {
+                randomVoting.Add(_rnd.Next(100));                
+            }
+            randomVoting = randomVoting.OrderByDescending(x=>x).ToList();
+            
+            //jokerDisplay.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            //for (int i = 0; i < count; i++)
+            //{
+            //    jokerDisplay.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            //}
+            for (int i = 0; i < count; i++)
+            {
+                int cnt = -1;
+                if (opti[i].IsCorrect)
+                {
+                    cnt = randomVoting[0];
+                    randomVoting.RemoveAt(0);
+                }
+                else
+                {                    
+                    if (randomVoting.Count == 1)
+                    {
+                        cnt = randomVoting[0];
+                        randomVoting.RemoveAt(0);
+                    }
+                    else {
+                        cnt = randomVoting[1];
+                        randomVoting.RemoveAt(1);
+                    }
+                }
+
+
+                //jokerDisplay.Children.Add(new Label() { Text = $"{i+1}: {cnt}" },0,i);
+                SKColor col;
+                switch (i % 6) {
+                    case 0: col = SKColor.Parse("#8590d5");break;
+                    case 1: col = SKColor.Parse("#266489");break;
+                    case 2: col = SKColor.Parse("#68B9C0");break;
+
+                    case 3: col = SKColor.Parse("#90D585"); break;
+                    case 4: col = SKColor.Parse("#d5ca85"); break;
+                    case 5: col = SKColor.Parse("#99ccdd"); break;
+                    default: col = SKColor.Parse("#99ccdd"); break;
+                }
+                 
+                
+                
+                entries.Add(new Microcharts.Entry(cnt) { Label = $"{i + 1}",Color=col });
+            }
+            ChartView ch = new ChartView() { HeightRequest = 150 };
+            ch.Chart = new BarChart() { Entries = entries,MaxValue=120 };
+            jokerDisplay.Children.Add(ch);
+        }
+
+        public void OnFiftyFifty() {
+            DisplayAlert("tuk", "50/50", "Cancel");
+            Jokers.Joker50on50--;
+            if (Jokers.Joker50on50 <= 0) {
+                fiftyfiftyImg.IsVisible = false;
+            }
+            var count = actualOptions.Count/2 - 1;
+            var list = actualOptions;
+            var srcList = new List<OptionInQuestionModel>();
+            foreach (var opt in list) {
+                if (opt.IsCorrect)
+                {
+                    srcList.Add(opt);
+                }
+                else
+                {
+                    if (count > 0)
+                    {
+                        srcList.Add(opt);
+                        count--;
+                    }
+                    
+                }
+                
+            }
+            actualOptions = srcList;
+            listOptions.ItemsSource = srcList;
         }
 
         public ICommand ItemClickCommand
@@ -62,6 +172,7 @@ namespace QuizMobileApp.View
 
         private void NextQuestion()
         {
+            jokerDisplay.Children.Clear();
             var quest = LevelViewModel.GetNextQuestion();
             if (quest == null)
             {
@@ -70,7 +181,8 @@ namespace QuizMobileApp.View
             else {
 
                 LblQuestion.Text = LevelViewModel.GetActualQuestion().QuestionText;
-                listOptions.ItemsSource = LevelViewModel.GetActualQuestion().Options.OrderBy((item) => _rnd.Next());
+                actualOptions = LevelViewModel.GetActualQuestion().Options.OrderBy((item) => _rnd.Next()).ToList();
+                listOptions.ItemsSource = actualOptions;
             }
         }
     }
